81ed668c09575b9315cd5cc4f7be6ffe6b121ec3
Bozhuk <bozhukihor1994@gmail.com>
2021-11-01 21:51:53 +0200

    2222

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,8 @@ $(function () {
         dots: true,
         infinite: true,
         speed: 500,
-        fade: true,
-        cssEase: 'linear'
+        cssEase: 'linear',
+        prevArrow: '<button class="slider-btn slider-btn-left"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
+        nextArrow: '<button class="slider-btn slider-btn-right"><img src="images/arrow-right.svg" alt="arrow-right"></button>'
     });
 })
