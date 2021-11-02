2aaffa6f6a0b1367f27afd0ba16a9472b2818cb7
Bozhuk <bozhukihor1994@gmail.com>
2021-11-02 11:33:29 +0200

    full desctop

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,13 @@ $(function () {
         prevArrow: '<button class="slider-btn slider-btn-left"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
         nextArrow: '<button class="slider-btn slider-btn-right"><img src="images/arrow-right.svg" alt="arrow-right"></button>'
     });
+    $(".questions__item-title").on("click", function () {
+        $(".questions__item").removeClass("questions__item--active");
+        $(this).parent().addClass("questions__item--active");
+    });
+    $("#fullpage").fullpage({
+        autoScrolling: true,
+        scrollHorizontally: true,
+        sectionSelector: '.page__section'
+    });
 })
